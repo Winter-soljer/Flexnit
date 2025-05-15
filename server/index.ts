a5d6031b72bfb00bea00fd0977b57688ff62c806
Winter-soljer <86882297+Winter-soljer@users.noreply.github.com>
2025-05-15 17:01:15 +0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,7 +1,12 @@
 import express, { type Request, Response, NextFunction } from "express";
 import path from "path"; 
+import { fileURLToPath } from "url"; // <-- Added
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+
+// Fix __dirname in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.json());
