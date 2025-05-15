f222028cc89c7388ab439b95ac1e39e8796d0097
Winter-soljer <86882297+Winter-soljer@users.noreply.github.com>
2025-05-15 06:48:12 +0400

    Update index.ts
    ads.txt update

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,16 @@
 import express, { type Request, Response, NextFunction } from "express";
+import path from "path"; 
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get("/ads.txt", (req: Request, res: Response) => {
+  res.type("text/plain");
+  res.sendFile(path.join(__dirname, "../ads.txt"));
+});
 
 app.use((req, res, next) => {
   const start = Date.now();
@@ -47,23 +53,21 @@ app.use((req, res, next) => {
     throw err;
   });
 
-  // importantly only setup vite in development and after
-  // setting up all the other routes so the catch-all route
-  // doesn't interfere with the other routes
   if (app.get("env") === "development") {
     await setupVite(app, server);
   } else {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client
   const port = 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
-    log(`serving on port ${port}`);
-  });
+  server.listen(
+    {
+      port,
+      host: "0.0.0.0",
+      reusePort: true,
+    },
+    () => {
+      log(`serving on port ${port}`);
+    }
+  );
 })();
